25117bc9923c630047b8218099f1db075971c415
unknown <brannongarrett77@gmail.com>
2024-01-15 11:22:51 +0100

    Added multiple models to models.py

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,27 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+# Auction listing model
+class Listing(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# Auction bid model
+class Bid(models.Model):
+    bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+# Auction comment model
+class Comment(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
