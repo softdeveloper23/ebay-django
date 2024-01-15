ea6e579240ae237ac956b25a885a39eb88c34009
unknown <brannongarrett77@gmail.com>
2024-01-15 21:59:44 +0100

    Can only get it to work for closing listings not adding highest bidders

--- auctions/models.py
+++ auctions/models.py
@@ -23,7 +23,7 @@ class Listing(models.Model):
 class Bid(models.Model):
     bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
 
     def __str__(self):
         return f"{self.bid.listing: self.bid_amount}"
@@ -44,8 +44,3 @@ class Watchlist(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s watchlist"
-
-class Bid(models.Model):
-    bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
